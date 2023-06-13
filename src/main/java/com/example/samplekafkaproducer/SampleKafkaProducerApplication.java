26d0beb5bf54311ceefaad0fe4da3cd5fa943fc1
ilteriscivelek <ilteris.dk.civelek@gmail.com>
2023-06-13 23:36:03 +0300

    initial commit

---
+++ src/main/java/com/example/samplekafkaproducer/SampleKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package com.example.samplekafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleKafkaProducerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/samplekafkaproducer/SampleKafkaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.samplekafkaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SampleKafkaProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
